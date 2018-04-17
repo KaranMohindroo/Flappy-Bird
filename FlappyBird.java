22aab77a4714385d00f8797b47e25f341ea5a36c
kaurarshdeep97 <30416023+kaurarshdeep97@users.noreply.github.com>
2018-04-17 16:35:40 +0530

    Edited Columns

--- FlappyBird.java
+++ FlappyBird.java
@@ -56,6 +56,23 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
 		timer.start();
   }
+  public void addColumn(boolean start)
+	{
+		int space = 300; // it is the space between two obstacle vertically
+		int width = 100; // it is the width of rectangular obstacle
+		int height = 50 + rand.nextInt(300); //height of rectangular obstacle will be decided randomly
+
+		if (start) // at the arart of game
+		{
+			columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT - height - 120, width, height));
+			columns.add(new Rectangle(WIDTH + width + (columns.size() - 1) * 300, 0, width, HEIGHT - height - space));
+		}
+		else
+		{
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT - height - 120, width, height));
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x, 0, width, HEIGHT - height - space));
+		}
+	}
   public void paintColumn(Graphics g, Rectangle column)
 	{
 		g.setColor(Color.green.darker());
